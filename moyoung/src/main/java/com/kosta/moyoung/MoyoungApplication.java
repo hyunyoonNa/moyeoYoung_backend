f1880c400685c708a7a33c0bca1d5c1404ad1e33
cjstmdgusqw <cjstmdgusqw@gmail.com>
2023-06-25 22:04:31 +0900

    moyoung project start

---
+++ moyoung/src/main/java/com/kosta/moyoung/MoyoungApplication.java
@@ -0,0 +1,13 @@
+package com.kosta.moyoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyoungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoyoungApplication.class, args);
+	}
+
+}
---
+++ moyoung/src/test/java/com/kosta/moyoung/MoyoungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.moyoung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyoungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
