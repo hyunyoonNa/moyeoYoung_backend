df128299150c0f82dc6f13e95cb20153e215af7b
cjstmdgusqw <96002337+cjstmdgusqw@users.noreply.github.com>
2023-07-10 15:27:17 +0900

    Delete PhotoRepository.java

--- moyoung/src/main/java/com/kosta/moyoung/feedroom/repository/PhotoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kosta.moyoung.feedroom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kosta.moyoung.feedroom.entity.PhotoEntity;
-
-public interface PhotoRepository extends JpaRepository<PhotoEntity, Long> {
-
-}
